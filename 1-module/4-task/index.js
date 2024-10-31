13d56bba9d344dd2168687b40a63bc707192f5c3
Katoshi12 <bulat.bulatov.99@bk.ru>
2024-10-31 23:27:16 +0300

    Реализовал функцию проверки на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
-  // ваш код...
+  const lowerText = str.toLowerCase();
+  if(lowerText.includes('x')) {
+    return true
+  } else if(lowerText.includes('xxx')) {
+    return true
+  } else {
+    return false
+  }
 }
+
+checkSpam()
