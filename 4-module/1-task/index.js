f78c54d97bf6beedccdd0ee4e8e50c27a29bc097
Katoshi12 <bulat.bulatov.99@bk.ru>
2024-11-18 00:16:22 +0300

    Преобразование массив в DOM элемент

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const list = document.createElement('ul');
+  for (let item of friends) {
+    const friendLists = document.createElement('li');
+    friendLists.innerHTML = ` ${ item.firstName }, ${item.lastName}`
+    list.appendChild(friendLists)
+  }
+  return list;
 }
