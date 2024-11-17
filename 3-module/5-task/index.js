3512830f4e30cbb08f73631fb1c199a8c154f315
Katoshi12 <bulat.bulatov.99@bk.ru>
2024-11-17 23:19:03 +0300

    Реализовал функцию min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,2 +1,11 @@
 function getMinMax(str) {
+    let elements = str.split(' ');
+
+    let numbers = elements
+        .map(item => parseFloat(item))
+        .filter(item => !isNaN(item));
+    return {
+        min: Math.min(...numbers),
+        max: Math.max(...numbers)
+    };
 }
